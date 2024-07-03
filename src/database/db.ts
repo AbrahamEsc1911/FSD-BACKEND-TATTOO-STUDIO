a40d480716866159701e10e69c401ab9bb9c5784
AEscobarAngola <abrancho1908@gmail.com>
2024-07-03 17:20:22 +0200

    feat: created migrations  of appointments

--- src/database/db.ts
+++ src/database/db.ts
@@ -4,6 +4,7 @@ import { DataSource } from "typeorm"
 import { Roles1720014722571 } from './migrations/1720014722571-roles'
 import { Users1719942765976 } from './migrations/1719942765976-users'
 import { Services1720015489582 } from './migrations/1720015489582-services'
+import { Appointments1720016459832 } from './migrations/1720016459832-appointments'
 export const AppDataSource = new DataSource({
 type: "mysql",
 host: process.env.DB_HOST,
@@ -12,7 +13,7 @@ username: process.env.DB_USERNAME,
 password: process.env.DB_PASSWORD,
 database: process.env.DB_DATABASE,
 entities: [],
-migrations: [Roles1720014722571, Users1719942765976, Services1720015489582],
+migrations: [Roles1720014722571, Users1719942765976, Services1720015489582, Appointments1720016459832],
 synchronize: false,
 logging: false,
 })
---
+++ src/database/migrations/1720016459832-appointments.ts
@@ -0,0 +1,66 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class Appointments1720016459832 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(new Table({
+            name: 'appointments',
+            columns: [
+                {
+                    name: 'id',
+                    type: 'int',
+                    isPrimary: true,
+                    isGenerated: true,
+                    generationStrategy: 'increment',
+                },
+                {
+                    name: 'users_id',
+                    type: 'int'
+                },
+                {
+                    name: 'services_id',
+                    type: 'int'
+                },
+                {
+                    name: 'is_active',
+                    type: 'Boolean',
+                    default: true,
+                },
+                {
+                    name: 'due_date',
+                    type: 'datetime',
+                    isNullable: false
+                },
+                {
+                    name: 'created_at',
+                    type: 'datetime',
+                    default: 'now()'
+                },
+                {
+                    name: 'updated_at',
+                    type: 'datetime',
+                    default: 'now()',
+                    onUpdate: 'now()'
+                }
+            ],
+            foreignKeys: [
+                {
+                    columnNames: ['users_id'],
+                    referencedTableName: 'users',
+                    referencedColumnNames: ['id']
+
+                },
+                {
+                    columnNames: ['services_id'],
+                    referencedTableName: 'services',
+                    referencedColumnNames: ['id']
+                },
+            ],
+        }))
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('appointments')
+    }
+
+}
