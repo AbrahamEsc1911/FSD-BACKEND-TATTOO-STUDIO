6de96f53debc15c6c825ecd800c71ba9f19185e7
AEscobarAngola <abrancho1908@gmail.com>
2024-07-25 21:17:20 +0200

    fix: solvin problen on middleware isSuperAdmin return lost

--- src/middlewares/isSuperAdmin.ts
+++ src/middlewares/isSuperAdmin.ts
@@ -4,7 +4,7 @@ export const isSuperAdmin = async (req: Request, res: Response, next: NextFuncti
     try {
 
         if (req.tokenData.role !== 3) {
-            res.json(
+            return res.status(400).json(
                 {
                     success: false,
                     message: 'you are not allowed'
